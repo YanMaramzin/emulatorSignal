6d8b634b5201f9e7a5b17219b21f04264d0e2f0c
mist <yan.maramzin@mail.ru>
2024-09-22 00:50:37 +0300

    [*] performance check

--- main.cpp
+++ main.cpp
@@ -1,17 +1,22 @@
 #include <QGuiApplication>
+#include <QApplication>
 #include <QQmlApplicationEngine>
+#include <qmlqcustomplot.h>
 
 int main(int argc, char *argv[])
 {
-    QGuiApplication app(argc, argv);
+    QApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
     const QUrl url(u"qrc:/EmulQml/main.qml"_qs);
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreationFailed, &app,
-        []()
-        {
-            QCoreApplication::exit(-1);
-        }, Qt::QueuedConnection);
+
+    qmlRegisterType<QmlQCustomPlot>("CustomPlot", 1, 0, "CustomPlot");
+
+    // QObject::connect(&engine, &QQmlApplicationEngine::objectCreationFailed, &app,
+    //     []()
+    //     {
+    //         QCoreApplication::exit(-1);
+    //     }, Qt::QueuedConnection);
     engine.load(url);
 
     return app.exec();
