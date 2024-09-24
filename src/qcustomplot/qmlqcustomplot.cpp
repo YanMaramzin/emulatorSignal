83e1fb67b93b7da3785949954083140d2ee8db76
mist <yan.maramzin@mail.ru>
2024-09-25 00:37:48 +0300

    [*] customplot qml

--- src/qcustomplot/qmlqcustomplot.cpp
+++ src/qcustomplot/qmlqcustomplot.cpp
@@ -3,20 +3,23 @@
 
 QmlQCustomPlot::QmlQCustomPlot(QQuickItem *parent) : QQuickPaintedItem(parent)
 {
+    setFlag( QQuickItem::ItemHasContents, true );
+    setAcceptedMouseButtons( Qt::AllButtons );
+
     connect(this, &QQuickPaintedItem::widthChanged, this, &QmlQCustomPlot::updateCustomPlotSize);
     connect(this, &QQuickPaintedItem::heightChanged, this, &QmlQCustomPlot::updateCustomPlotSize);
 }
 
 QmlQCustomPlot::~QmlQCustomPlot()
 {
-    if (m_timerId != 0)
+    if(m_timerId != 0) {
         killTimer(m_timerId);
+    }
 }
 
 void QmlQCustomPlot::initCustomPlot()
 {
     m_customPlot = std::make_unique<QCustomPlot>();
-    qDebug() << "initCustomPlot";
     updateCustomPlotSize();
     m_customPlot->addGraph();
     m_customPlot->graph(0)->setPen(QPen(Qt::red));
@@ -33,6 +36,26 @@ void QmlQCustomPlot::initCustomPlot()
     m_customPlot->replot();
 }
 
+void QmlQCustomPlot::routeMouseEvents(QMouseEvent *event)
+{
+    if (!m_customPlot)
+        return;
+
+    // QMouseEvent* newEvent = new QMouseEvent( event->type(), event->localPos(), event->button(), event->buttons(), event->modifiers() );
+    // QCoreApplication::postEvent(m_customPlot.get(), newEvent );
+
+}
+
+void QmlQCustomPlot::routeWheelEvents(QWheelEvent *event)
+{
+    if (!m_customPlot)
+        return;
+
+    // QWheelEvent* newEvent = new QWheelEvent(event->position(), event->globalPosition(), event->pixelDelta(), event->angleDelta(), event->buttons(), event->modifiers(), event->phase(), event->inverted());
+    // QCoreApplication::postEvent(m_customPlot.get(), newEvent);
+
+}
+
 void QmlQCustomPlot::timerEvent(QTimerEvent *event)
 {
     static double t, U;
@@ -41,6 +64,31 @@ void QmlQCustomPlot::timerEvent(QTimerEvent *event)
     qDebug() << Q_FUNC_INFO << QString("Adding dot t = %1, S = %2").arg(t).arg(U);
     t++;
     m_customPlot->replot();
+}
+
+void QmlQCustomPlot::mousePressEvent(QMouseEvent *event)
+{
+    routeMouseEvents(event);
+}
+
+void QmlQCustomPlot::mouseReleaseEvent(QMouseEvent *event)
+{
+    routeMouseEvents(event);
+}
+
+void QmlQCustomPlot::mouseMoveEvent(QMouseEvent *event)
+{
+    routeMouseEvents(event);
+}
+
+void QmlQCustomPlot::mouseDoubleClickEvent(QMouseEvent *event)
+{
+    routeMouseEvents(event);
+}
+
+void QmlQCustomPlot::wheelEvent(QWheelEvent *event)
+{
+    routeWheelEvents(event);
 }
 
 void QmlQCustomPlot::updateCustomPlotSize() const
--- src/qcustomplot/qmlqcustomplot.h
+++ src/qcustomplot/qmlqcustomplot.h
@@ -14,14 +14,25 @@ public:
 
     ~QmlQCustomPlot() override;
 
-    void paint(QPainter *painter);
+    void paint(QPainter *painter) override;
 
     Q_INVOKABLE void initCustomPlot();
 protected:
+    void routeMouseEvents(QMouseEvent *event);
+    void routeWheelEvents(QWheelEvent *event);
+
     virtual void timerEvent(QTimerEvent *event) override;
+
+    virtual void mousePressEvent(QMouseEvent *event) override;
+    virtual void mouseReleaseEvent(QMouseEvent *event) override;
+    virtual void mouseMoveEvent(QMouseEvent *event) override;
+    virtual void mouseDoubleClickEvent(QMouseEvent *event) override;
+    virtual void wheelEvent(QWheelEvent *event) override;
+
 private:
     std::unique_ptr<QCustomPlot> m_customPlot {nullptr};
     int m_timerId {0};
+    void graphClicked(QCPAbstractPlottable *plottable);
     void updateCustomPlotSize() const;
     void onCustomReplot();
 };
