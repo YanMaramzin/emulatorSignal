f33860d00569e531bb35f0420fa4c855a36fe530
mist <yan.maramzin@mail.ru>
2024-10-04 01:26:59 +0300

    [*] added duration in customgeneratorparametrs

--- src/lib/generator/customgeneratorparametrs.h
+++ src/lib/generator/customgeneratorparametrs.h
@@ -9,14 +9,17 @@ public:
     double frequency() const;
     double amplitude() const;
     double phase() const;
+    double duration() const;
     double step() const;
 
     void setFrequency(double frequency);
     void setAmplitude(double amplitude);
+    void setDuration(double value);
 
 private:
     double m_frequency {100.0};
     double m_amplitude {50.0};
     double m_phase {0.0};
-    double m_step {1 / 1000.0};
+    double m_duration {0.0};
+    double m_step {1 / 10000.0};
 };
